0b1338810534027daa81a354cf0f440f7ea9d280
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-05-09 16:10:17 +0530

    Add authentication middleware to verify user tokens

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,21 @@
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const authenticateUser = async (req, res, next) => {
+  const token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decodedToken._id).select("-password");
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    console.log("Error in authentication middleware", err);
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
