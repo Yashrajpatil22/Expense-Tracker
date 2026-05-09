e81f00e2610ee544ce629d874d1d94cbc098a075
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-05-09 13:07:31 +0530

    Add user registration controller with validation and token generation

---
+++ src/controllers/User.controller.js
@@ -0,0 +1,25 @@
+import express from "express";
+import User from "../models/User.model.js";
+
+const registerUser = async (req, res) => {
+
+    const { email, password } = req.body;
+    if(!email || !password || email.trim() === "" || password.trim() === ""){
+        return res.status(400).json({message: "Email and password are required"});
+    }
+    try{
+        const existingUser  = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message: "User already exists"});
+        }
+        const user = await User.create({email, password});
+        const token = user.generateToken();
+
+        return res.status(201).json({message: "User registered successfully", user: {email: user.email, _id: user._id}, token});
+    }
+    catch(err){
+        console.log("Error registering user", err);
+        return res.status(500).json({message: "Internal server error"});
+    }
+
+}
