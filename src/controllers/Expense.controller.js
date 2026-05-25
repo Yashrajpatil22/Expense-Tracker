9792662331978424a6834268a466f61cfe02e375
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-05-25 18:08:53 +0530

    Add update and delete functionality for expenses with error handling

--- src/controllers/Expense.controller.js
+++ src/controllers/Expense.controller.js
@@ -28,10 +28,72 @@ const getExpenses = async (req, res) => {
       .status(200)
       .json({ message: "Expenses fetched successfully", data });
   } catch (error) {
-    console.log("Something went wrong while fetching expenses",error);
-    return res.status(500).json({message: "Something went wrong while fetching expenses"})
-    
+    console.log("Something went wrong while fetching expenses", error);
+    return res
+      .status(500)
+      .json({ message: "Something went wrong while fetching expenses" });
   }
 };
 
-export { createExpense, getExpenses };
+const updateExpense = async (req, res) => {
+  const { title, category, amount, date } = req.body;
+  const { expenseId } = req.params;
+  if (!title && !category && !amount && !date) {
+    return res.status(400).json({ message: "Atleast one field is required" });
+  }
+  try {
+    const expense = await Expense.findById(expenseId);
+    if (!expense) {
+      return res
+        .status(404)
+        .json({ message: "Expense with this id not found" });
+    }
+    if (!expense.owner.equals(req.user._id)) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to perform this action" });
+    }
+    if (title && title?.trim() != "") {
+      expense.title = title.trim();
+    }
+    if (amount || amount === 0) {
+      expense.amount = amount;
+    }
+    if (category) {
+      expense.category = category;
+    }
+    if (date) {
+      expense.date = date;
+    }
+    await expense.save();
+
+    return res.status(200).json({ message: "Updation successful", expense });
+  } catch (error) {
+    console.log("Failed to update", error);
+    return res.status(500).json({ message: "Failed to update" });
+  }
+};
+
+const deleteExpense = async (req, res) => {
+  const { expenseId } = req.params;
+  try {
+    const expense = await Expense.findById(expenseId);
+    if (!expense) {
+      return res.status(404).json({ message: "Expense with the id not found" });
+    }
+    if (!expense.owner.equals(req.user._id)) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to perform this action" });
+    }
+    await Expense.deleteOne({ _id: expenseId });
+    return res.status(200).json({ message: "Expense deleted successfully" });
+  } catch (error) {
+    console.log("Something went wrong while deleting expense", error);
+    return res
+      .status(500)
+      .json({ message: "Something went wrong while deleting expense" });
+  }
+};
+
+export { createExpense, getExpenses, updateExpense, deleteExpense };
--- src/routes/Expense.route.js
+++ src/routes/Expense.route.js
@@ -1,16 +1,21 @@
-import express from 'express';
+import express from "express";
 import {
   createExpense,
   getExpenses,
+  updateExpense,
+  deleteExpense,
 } from "../controllers/Expense.controller.js";
-import { authenticateUser } from '../middlewares/auth.middleware.js';
+import { authenticateUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.route("/create")
-  .post(authenticateUser, createExpense);
+router.route("/create").post(authenticateUser, createExpense);
 
-router.route("/get-expenses")
-  .get(authenticateUser,getExpenses)
+router.route("/get-expenses").get(authenticateUser, getExpenses);
+
+router.route("/update-expense/:expenseId").put(authenticateUser, updateExpense);
+
+router.route("/delete-expense/:expenseId").delete(authenticateUser,deleteExpense)
+
 
 export default router;
