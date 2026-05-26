9d2dc9263242f7ae7f98624ef2407452f3061f8b
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-05-26 18:56:59 +0530

    feat: implement routing and add pages for Login, Register, Dashboard, and EditExpense

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Dashboard from "./pages/Dashboard"
+import EditExpense from "./pages/EditExpense"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-    </>
+  
+return (
+  <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/dashboard" element={<Dashboard />} />
+    <Route path="/edit-expense/:expenseId" element={<EditExpense />} />
+  </Routes>
   )
 }
+
 
 export default App
