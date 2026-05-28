7289e1716b5d59999cc2eac7acbd01d221b617f1
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-05-28 12:26:06 +0530

    feat: implement expense fetching and display in Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import ProtectedRoute from '../components/ProtectedRoute';
+import React, { useState, useEffect } from "react";
+import ProtectedRoute from "../components/ProtectedRoute";
+import axios from "axios";
 
 function Dashboard() {
+  const [expenses, setExpenses] = useState([]);
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      const token = localStorage.getItem("token");
+      try {
+        const response = await axios.get(
+          "http://localhost:3001/api/expenses/get-expenses",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+        setExpenses(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchExpenses();
+  }, []);
+
   return (
     <ProtectedRoute>
-      <div>Dashboard</div>
+      <div>
+        <div>Dashboard</div>
+        {expenses.map((expense) => {
+          return (
+            <div key={expense._id}>
+              <h2>{expense.title}</h2>
+              <h3>{expense.amount}</h3>
+              <h3>{expense.category}</h3>
+              <h3>{new Date(expense.date).toLocaleDateString()}</h3>
+            </div>
+          );
+        })}
+      </div>
     </ProtectedRoute>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
