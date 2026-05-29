21bdf60fe89935182144b3f8190c4c2c7a89d9c0
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-05-29 20:01:23 +0530

    feat: implement expense submission functionality in AddExpense page

--- frontend/src/pages/AddExpense.jsx
+++ frontend/src/pages/AddExpense.jsx
@@ -1,19 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function AddExpense() {
+    const[title,setTitle] = useState("");
+    const[amount,setAmount] = useState(0);
+    const[category,setCategory] = useState("Groceries");
+    const [date, setDate] = useState(
+      new Date().toISOString().split("T")[0],
+    );
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+          const token = localStorage.getItem("token");
+          const response = await axios.post("http://localhost:3001/api/expenses", {
+            title,
+            amount,
+            category,
+            date
+          }, {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+          navigate('/dashboard');
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
   return (
     <div className="flex items-center justify-center min-h-screen bg-slate-950">
       <div className="border-slate-700 border-2 rounded-2xl p-8 bg-slate-900 flex flex-col gap-4 w-96">
         <h2 className="text-white text-2xl font-bold text-center">
           Add Expense
         </h2>
-        <form className="flex flex-col gap-4">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <div className="flex flex-col gap-1">
             <label className="text-slate-300">Title:</label>
             <input
               type="text"
               placeholder="Title"
               className="bg-slate-800 text-white border border-slate-700 rounded-md mx-2 p-1"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
             />
           </div>
           <div className="flex flex-col gap-1">
@@ -22,11 +54,16 @@ function AddExpense() {
               type="number"
               placeholder="Amount"
               className="bg-slate-800 text-white border border-slate-700 rounded-md mx-2 p-1"
+              value={amount}
+              onChange={(e) => setAmount(Number(e.target.value))}
             />
           </div>
           <div className="flex flex-col gap-1">
             <label className="text-slate-300">Category:</label>
-            <select className="bg-slate-800 text-white border border-slate-700 rounded-md mx-2 p-1">
+            <select className="bg-slate-800 text-white border border-slate-700 rounded-md mx-2 p-1"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
               <option value="Groceries">Groceries</option>
               <option value="Leisure">Leisure</option>
               <option value="Electronics">Electronics</option>
@@ -41,6 +78,8 @@ function AddExpense() {
             <input
               type="date"
               className="bg-slate-800 text-white border border-slate-700 rounded-md mx-2 p-1"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
             />
           </div>
           <button type="submit" className="bg-emerald-500 hover:bg-emerald-600 text-white p-1 rounded-md">Add Expense</button>
