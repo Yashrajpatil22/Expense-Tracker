abf72fc68527b0f20b44ddc726de90735db9ca20
Yashraj Patil <yashrajpatil2206@gmail.com>
2026-06-15 17:05:07 +0530

    feat: add redirect from root path to login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
@@ -10,6 +10,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route
@@ -20,16 +21,22 @@ function App() {
           </ProtectedRoute>
         }
       />
-      <Route path="/edit-expense/:expenseId" element={
-        <ProtectedRoute>
-          <EditExpense />
-        </ProtectedRoute>
-      } />
-      <Route path="/add-expense" element={
-        <ProtectedRoute>
-          <AddExpense />
-        </ProtectedRoute>
-      } />
+      <Route
+        path="/edit-expense/:expenseId"
+        element={
+          <ProtectedRoute>
+            <EditExpense />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/add-expense"
+        element={
+          <ProtectedRoute>
+            <AddExpense />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
